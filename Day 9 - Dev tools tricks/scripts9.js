80cda3695e8a66d3910848a7ec47ebd3ac54b518
David Pérez <dperezz@unal.edu.co>
2023-01-07 17:32:24 -0500

    Days 8 & 9

---
+++ Day 9 - Dev tools tricks/scripts9.js
@@ -0,0 +1,47 @@
+const dogs = [
+    {name: "Snickers", age: 2},
+    {name: "hugo", age: 8}
+]
+
+function MakeGreen() {
+    const p = document.querySelector("p");
+    p.style.color = "#BADA55";
+    p.style.fontSize = "50px";
+}
+
+console.log("normal");
+console.log("Aquí puedo poner lo que %s", "papaya"); // "Aquí puedo poner lo que papaya
+console.log("%c Soy un texto grande", "font-size: 25px; background: red"); // el texto se imprimirá en consola con las propiedades especificadas
+
+console.table(dogs);
+
+console.warn("Oh no :c");
+console.error("Sh*t!");
+
+console.info("Un cocodrilo se come unas 3-4 personas por año");
+
+console.assert(1 === 1, "Se equivoca"); // no imprime nada
+console.assert(1 === 2, "Se equivoca"); // imprime un error con el mensaje "Se equivoca"
+
+const p = document.querySelector('p');
+console.dir(p) // muestra una lista con atributos, métodos y demás de dicho objeto
+
+dogs.forEach(dog => {
+	console.group(`${dog.name}`);
+	console.log(`Hola, este es ${dog.name}.`);
+	console.log(`${dogs.name} tiene ${dog.age} años.`);
+	console.groupEnd(`${dog.name}`);
+});
+
+console.count('a'); // imrpime 1
+console.count('a'); // imrpime 2
+console.count('b'); // imrpime 1
+console.count('a'); // imrpime 3
+
+console.time("Esperando...");
+fetch("https://api.github.com/users/wesbos")
+    .then(data => data.json())
+    .then(data => {
+        console.timeEnd("Esperando...");
+        console.log(data);
+    });
